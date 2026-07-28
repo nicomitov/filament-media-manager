0869b6784a25185203386f981bd2cd7ba9818daa
nicomitov <nicomitov@gmail.com>
2026-07-28 17:29:44 +0300

    pagination options changed

--- resources/views/filament/pages/media-manager/pagination.blade.php
+++ resources/views/filament/pages/media-manager/pagination.blade.php
@@ -2,7 +2,7 @@
     <x-filament::pagination
         :paginator="$paginator"
         {{-- :page-options="[10, 25, 50, 100, 'all']" --}}
-        :page-options="[12, 18, 25, 50, 100, 200]"
+        :page-options="[12, 18, 25, 50, 100]"
         current-page-option-property="perPage"
         :extreme-links="true" />
 </div>
